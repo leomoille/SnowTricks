58af1ca302694e2080fc55dcb9ba073d400c4df5
Leo Moille <leomoille18@gmail.com>
2022-04-01 10:43:01 +0200

    snowtricks-#1 Add Bootstrap
    - Add favicon
    - Add Bootstrap Style and JavaScript
    - Add Custom Bootstrap CSS
    - Add navbar and footer

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+import './styles/bootstrap.min.css';
+import './styles/bootstrap-sketch.min.css';
 import './styles/app.css';
 
 // start the Stimulus application
-import './bootstrap';
+import './stimulus';
+
+// import bootstrap custom js
+import './bootstrap.bundle.min.js';
+import './bootstrap-custom';
