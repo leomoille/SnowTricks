0048d8759ea37311310c865ce86726ec7aacda8a
Léo Moille <leomoille18@gmail.com>
2022-06-28 20:12:40 +0200

    #10 - add user password reset

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ForgotPasswordType;
 use App\Form\RegistrationType;
 use App\Form\ResetPasswordType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,6 +15,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -62,47 +66,104 @@ class SecurityController extends AbstractController
     /**
      * @Route("/mot-de-passe-oublie", name="security_forgot_password")
      */
-    public function forgotPassword(Request $request, MailerInterface $mailer): Response
-    {
-        $form = $this->createForm(ResetPasswordType::class);
+    public function forgotPassword(
+        Request $request,
+        MailerInterface $mailer,
+        UserRepository $userRepository,
+        TokenGeneratorInterface $tokenGenerator,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $form = $this->createForm(ForgotPasswordType::class);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dump($form->getData()['email']);
+            $user = $userRepository->findOneByEmail($form->get('email')->getData());
 
-            if ($form->getData()['email'] == $form->getData()['confirmEmail']) {
+            if ($user) {
+                $token = $tokenGenerator->generateToken();
+                $user->setResetToken($token);
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $url = $this->generateUrl(
+                    'security_reset_password',
+                    ['token' => $token],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                );
+
+                $email = (new TemplatedEmail())
+                    ->from('no-reply@snowtricks.dev')
+                    ->to($user->getEmail())
+                    ->subject('Réinitialisation de mot de passe')
+                    ->htmlTemplate('email/password-recovery.html.twig')
+                    ->context([
+                        'url' => $url,
+                        'user' => $user,
+                    ]);
+
+                $mailer->send($email);
+
                 $this->addFlash(
                     'notice',
                     'Top, tu vas bientôt pouvoir retrouver ton compte !'
                 );
-                $email = (new TemplatedEmail())
-                    ->from('no-reply@snowtricks.dev')
-                    ->to('you@example.com')
-                    ->subject('Mot de passe oublié ?')
-                    ->htmlTemplate('email/password-recovery.html.twig')
-                    ->context([
-                        'username' => 'Léo',
-                    ]);
-
-                /*
-                $dsn = 'smtp://0.0.0.0:1025';
-                $transport = Transport::fromDsn($dsn);
-                $mailer = new Mailer($transport);
-                 */
-                $mailer->send($email);
             } else {
-                if ($form->getData()['email'] != $form->getData()['confirmEmail']) {
-                    $this->addFlash(
-                        'warning',
-                        'Les deux mails ne correspondent pas...'
-                    );
-                }
+                $this->addFlash(
+                    'warning',
+                    'Désolé mais personne n\'a été trouvé avec ce mail...'
+                );
+
+                return $this->redirectToRoute('security_forgot_password');
             }
         }
 
         return $this->render('security/forgot-password.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/mot-de-passe-oublie/{token}", name="security_reset_password")
+     */
+    public function resetPassword(
+        string $token,
+        Request $request,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $user = $userRepository->findOneByResetToken($token);
+
+        if ($user) {
+            $form = $this->createForm(ResetPasswordType::class);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                dump($form->get('password')->getData());
+                $user->setResetToken('');
+                $user->setPassword(
+                    $passwordHasher->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Ton mot de passe à bien été changé !');
+
+                return $this->redirectToRoute('security_login');
+            }
+
+            return $this->render('security/reset-password.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+
+        $this->addFlash('warning', 'Jeton invalide');
+
+        return $this->redirectToRoute('security_forgot_password');
     }
 }
--- src/Form/ForgotPasswordType.php
+++ src/Form/ForgotPasswordType.php
@@ -3,16 +3,16 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ResetPasswordType extends AbstractType
+class ForgotPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', PasswordType::class);
+            ->add('email', EmailType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
