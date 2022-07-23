a274569a8a0119d4ebf189ef1e6f986c2f8a44f6
Léo Moille <leomoille18@gmail.com>
2022-07-23 09:27:55 +0200

    Add VideoFixtures

--- src/DataFixtures/VideoFixtures.php
+++ src/DataFixtures/VideoFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\DataFixtures\TrickFixtures;
+use App\Entity\Video;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class VideoFixtures extends Fixture
+class VideoFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $videoLink = 'https://www.youtube.com/watch?v=ScMzIvxBSi4';
+
+        for ($i = 0; $i < 12; $i++) {
+            $video = new Video();
+
+            $video->setUrl($videoLink)
+                ->setTrick($this->getReference(TrickFixtures::TRICK_REFERENCE[$i]));
+
+            $manager->persist($video);
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TrickFixtures::class
+        ];
     }
 }
