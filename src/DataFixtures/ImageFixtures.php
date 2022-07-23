bf3483471c1c9aba44182e249ceb7663228fe0f0
Léo Moille <leomoille18@gmail.com>
2022-07-23 09:46:42 +0200

    Add ImageFixtures and update template

--- src/DataFixtures/ImageFixtures.php
+++ src/DataFixtures/ImageFixtures.php
@@ -2,16 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ImageFixtures extends Fixture
+class ImageFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $images = ['snow.jpg', 'snow-2.jpg'];
+
+        for ($i = 0; $i < 12; $i++) {
+
+            for ($j = 0; $j < count($images); $j++) {
+                $image = new Image();
+
+                if ($j == 0) {
+                    $image->setIsFeatured(true);
+                }
+
+                $image
+                    ->setFilename($images[$j])
+                    ->setTrick($this->getReference(TrickFixtures::TRICK_REFERENCE[$i]));
+                $manager->persist($image);
+            }
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TrickFixtures::class
+        ];
     }
 }
