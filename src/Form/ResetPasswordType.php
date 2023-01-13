1519b972964d68c89f6d6d9f5e15fd40f4f02b76
Léo Moille <leomoille18@gmail.com>
2023-01-13 12:10:22 +0100

    Add password length check when new password is set

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -6,13 +6,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ResetPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', PasswordType::class);
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                    'min' => '8',
+                ],
+                'constraints' => [
+                    new Length(null, 8),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
