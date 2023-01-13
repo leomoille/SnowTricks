88d8fc3f030c2120a8cb93a228bf3706207a04f5
Léo Moille <leomoille18@gmail.com>
2023-01-13 12:19:22 +0100

    refactor

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 class TrickType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
